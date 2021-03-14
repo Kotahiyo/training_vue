3601ad2f98a51e5d02e7268243b3a39e9916a3e9
Kouhei <hiyotihakou@gmail.com>
2021-03-15 05:00:45 +0900

    v-for の使い方を学習

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <h1>Vue Tutorial</h1>
-    <input type="text" @input="onInput" />
-    <!-- <input type="text" v-model="message"> -->
+    <input type="text" v-model="message">
     <br>
     {{ message }}
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id" class="task-card">
+        {{ todo.text }}
+      </li>
+    </ul>
+
   </div>
 </template>
 
@@ -14,18 +20,22 @@ export default {
   name: 'App',
   data: function () {
     return {
-      message: 'ハロー'
+      message: 'ハロー',
+      todos: [
+        { id: 0 , text: 'Rails も勉強しないと'},
+        { id: 1 , text: '自動整形ツールを設定してみたい'},
+        { id: 2 , text: 'Vue は今週中まで'},
+      ],
     }
   },
-  methods: {
-    onInput: function (event) {
-      const text = event.target.value;
-      this.message = text;
-    },
-  }
 };
 </script>
 
 <style>
-
+.task-card {
+ border: solid 1px;
+ margin: 5px;
+ padding: 2px;
+ width: 250px;
+}
 </style>
